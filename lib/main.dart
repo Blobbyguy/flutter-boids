62fde0ba2d76912979ff9bc89518f115216a8e07
Blobbyguy <sam.gillard44@gmail.com>
2025-01-15 16:05:30 +1100

    Boids

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,285 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BoidsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BoidsApp extends StatelessWidget {
+  const BoidsApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Boids Simulation',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BoidsSimulation(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BoidsSimulation extends StatefulWidget {
+  const BoidsSimulation({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _BoidsSimulationState createState() => _BoidsSimulationState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BoidsSimulationState extends State<BoidsSimulation> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  final List<Boid> _boids = [];
+  double _numBoids = 100;
+  double _separationMultiplier = 1.0;
+  double _alignmentMultiplier = 1.0;
+  double _cohesionMultiplier = 1.0;
+  Size? _widgetSize;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 16))
+      ..addListener(_update)
+      ..repeat();
+
+    _initializeBoids();
+  }
+
+  void _initializeBoids() {
+    _boids.clear();
+
+    if (_widgetSize == null){
+      return;
+    }
+
+    for (int i = 0; i < _numBoids; i++) {
+      _boids.add(Boid(
+        position: Offset(Random().nextDouble() * _widgetSize!.width, Random().nextDouble() * _widgetSize!.height),
+        velocity: Offset(Random().nextDouble() * 2 - 1, Random().nextDouble() * 2 - 1),
+      ));
+    }
+  }
+
+  void _update() {
+    if (_widgetSize == null) {
+      return;
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      for (var boid in _boids) {
+        boid.update(_boids, _widgetSize!, _separationMultiplier, _alignmentMultiplier, _cohesionMultiplier);
+      }
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Boids Simulation'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          _buildSliders(),
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                _widgetSize = Size(constraints.maxWidth, constraints.maxHeight);
+                return CustomPaint(
+                  painter: BoidsPainter(_boids),
+                  child: Container(),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildSliders() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        _buildSlider('Number of Boids', _numBoids, 10, 200, (value) {
+          setState(() {
+            _numBoids = value;
+            _initializeBoids();
+          });
+        }, intOnly: true),
+        _buildSlider('Separation', _separationMultiplier, 0, 1, (value) {
+          setState(() {
+            _separationMultiplier = value;
+          });
+        }),
+        _buildSlider('Alignment', _alignmentMultiplier, 0, 1, (value) {
+          setState(() {
+            _alignmentMultiplier = value;
+          });
+        }),
+        _buildSlider('Cohesion', _cohesionMultiplier, 0, 1, (value) {
+          setState(() {
+            _cohesionMultiplier = value;
+          });
+        }),
+      ],
+    );
+  }
+
+  Widget _buildSlider(String label, double value, double min, double max, ValueChanged<double> onChanged, {bool intOnly = false}) {
+    return Row(
+      children: [
+        Text('$label: ${value.toStringAsFixed(2)}'),
+        Expanded(
+          child: Slider(
+            value: value,
+            min: min,
+            max: max,
+            divisions: intOnly ? (max - min).toInt() : null,
+            onChanged: onChanged,
+          ),
+        ),
+      ],
+    );
+  }
+}
+class Boid {
+  Offset position;
+  Offset velocity;
+
+  Boid({required this.position, required this.velocity});
+
+  void update(List<Boid> boids, Size screenSize, double separationMultiplier, double alignmentMultiplier, double cohesionMultiplier) {
+    final separation = _separation(boids) * separationMultiplier;
+    final alignment = _alignment(boids) * alignmentMultiplier;
+    final cohesion = _cohesion(boids) * cohesionMultiplier;
+    final boundaryAvoidance = _boundaryAvoidance(screenSize);
+
+    velocity += separation + alignment + cohesion + boundaryAvoidance;
+    velocity = Offset.fromDirection(velocity.direction, min(velocity.distance, 2.0));
+    position += velocity;
+  }
+
+  Offset _separation(List<Boid> boids) {
+    const double desiredSeparation = 25.0;
+    Offset steer = Offset.zero;
+    int count = 0;
+
+    for (var other in boids) {
+      final distance = (position - other.position).distance;
+      if (distance > 0 && distance < desiredSeparation) {
+        steer += (position - other.position) / distance;
+        count++;
+      }
+    }
+
+    if (count > 0) {
+      steer /= count.toDouble();
+    }
+
+    return steer;
+  }
+
+  Offset _alignment(List<Boid> boids) {
+    const double neighborDist = 50.0;
+    Offset sum = Offset.zero;
+    int count = 0;
+
+    for (var other in boids) {
+      final distance = (position - other.position).distance;
+      if (distance > 0 && distance < neighborDist) {
+        sum += other.velocity;
+        count++;
+      }
+    }
+
+    if (count > 0) {
+      sum /= count.toDouble();
+      sum = Offset.fromDirection(sum.direction, min(sum.distance, 2.0));
+      return (sum - velocity) / 8.0;
+    }
+
+    return Offset.zero;
+  }
+
+  Offset _cohesion(List<Boid> boids) {
+    const double neighborDist = 50.0;
+    Offset sum = Offset.zero;
+    int count = 0;
+
+    for (var other in boids) {
+      final distance = (position - other.position).distance;
+      if (distance > 0 && distance < neighborDist) {
+        sum += other.position;
+        count++;
+      }
+    }
+
+    if (count > 0) {
+      sum /= count.toDouble();
+      return (sum - position) / 100.0;
+    }
+
+    return Offset.zero;
+  }
+
+  Offset _boundaryAvoidance(Size screenSize) {
+    const double margin = 15.0;
+    const double turnFactor = 1.0;
+    Offset steer = Offset.zero;
+
+    if (position.dx < margin) {
+      steer += Offset(turnFactor, 0);
+    } else if (position.dx > screenSize.width - margin) {
+      steer += Offset(-turnFactor, 0);
+    }
+
+    if (position.dy < margin) {
+      steer += Offset(0, turnFactor);
+    } else if (position.dy > screenSize.height - margin) {
+      steer += Offset(0, -turnFactor);
+    }
+
+    return steer;
+  }
+}
+
+class BoidsPainter extends CustomPainter {
+  final List<Boid> boids;
+
+  BoidsPainter(this.boids);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
+    for (var boid in boids) {
+      canvas.drawCircle(boid.position, 3.0, paint);
+    }
+
+    // Draw boundary box
+    final boundaryPaint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.width, size.height),
+      boundaryPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
